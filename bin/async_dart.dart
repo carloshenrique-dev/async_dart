f4cc8634b82386f4090eeb2e6d7b0eba48a2456d
Carlos Henrique <c.tarabal25@gmail.com>
2021-07-05 22:28:12 -0300

    first commit

---
+++ bin/async_dart.dart
@@ -0,0 +1,5 @@
+import 'package:async_dart/async_dart.dart' as async_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${async_dart.calculate()}!');
+}
---
+++ test/async_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:async_dart/async_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
